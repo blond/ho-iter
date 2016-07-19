bba5cc794095ca41a43475853490c3f137f964cd
blond <andrewblond@yandex.com>
2016-07-19 10:20:29 +0300

    feat(empty): add empty method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
+const empty = require('./lib/empty');
 const series = require('./lib/series');
 const evenly = require('./lib/evenly');
 
-module.exports = { series, evenly };
+module.exports = { empty, series, evenly };
