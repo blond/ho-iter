46ecf8fb87d562f77eb26123f61f670a8242e581
blond <andrewblond@yandex.com>
2016-08-12 17:52:33 +0300

    feat(reverse): add `reverse()` method

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ exports.isIterable = require('./lib/is-iterable');
 exports.isIterator = require('./lib/is-iterator');
 exports.series = require('./lib/series');
 exports.evenly = require('./lib/evenly');
+exports.reverse = require('./lib/reverse');
 
 module.exports = exports;
